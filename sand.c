78a442328087f15fbfa23b7adaee8cee4cc50fbb
grantal <AlexV348@gmail.com>
2016-12-07 10:29:01 -0800

    prints out a 2d array

--- sand.c
+++ sand.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+
+#define HEIGHT_MAX 16
+
+// Prints out a 2d array
+void print2DArray(int l, int w, int a[l][w]){
+    for (int i = 0; i < l; i++){
+        for(int j = 0; j < w; j++){
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int l = 3; // length
+    int w = 3; // width
+    int before[l][w];
+    // set all elements to max
+    for (int i = 0; i < l; i++){
+        for(int j = 0; j < w; j++){
+            before[i][j] = HEIGHT_MAX;
+        }
+    }
+    
+
+    print2DArray(l, w, before);
+
+}
